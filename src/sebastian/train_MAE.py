e8ffc27e0972308a0d477e2e86a9c36bd54a070a
mmmmimic <matthewlin98@hotmail.com>
2024-08-13 18:08:56 +0200

    .

--- src/sebastian/train_MAE.py
+++ src/sebastian/train_MAE.py
@@ -52,7 +52,7 @@ class TR(nn.Module):
         image = self.image_embder(image).flatten(-3).permute(0,2,1); # B, N, C
         image = image + self.image_pos;
         vtx = self.vtx_embder(vtx); # B, N, C
-        vtx = nn.AdaptiveAvgPool1d(27)(vtx, dim=1) + nn.AdaptiveMaxPool1d(27)(vtx, dim=1);
+        vtx = nn.AdaptiveAvgPool1d(27)(vtx) + nn.AdaptiveMaxPool1d(27)(vtx);
         vtx = vtx + self.vtx_pos;
         seg = self.seg_embder(seg).flatten(-3).permute(0,2,1); # B, N, C
         seg = seg + self.seg_pos;
