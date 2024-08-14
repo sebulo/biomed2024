5ebe28febd7c231e8c6eda7cd294307832734f8c
mmmmimic <matthewlin98@hotmail.com>
2024-08-14 10:39:22 +0200

    add test tr

---
+++ src/sebastian/create_sub.py
@@ -0,0 +1,17 @@
+# create submissions
+import json
+import numpy as np
+
+if __name__=="__main__":
+    # probs = np.load('prob.npy')
+    with open('../results/test_files_200.txt', 'r') as f:
+        lines = f.read()
+    lines = lines.split('\n')[:-1];
+    for i, line in enumerate(lines, 0):
+        prob = probs[i];
+        d = {
+        "scan_id": lines,
+        "outlier": 0,
+        "outlier_probability": 0.29324252683460816,
+        "outlier_threshold": 0.392965204318129
+    },
