0c337ca577777d00ef61efab7f42943b21633971
mmmmimic <matthewlin98@hotmail.com>
2024-08-14 11:25:43 +0200

    add test tr

--- src/sebastian/test_MAE.py
+++ src/sebastian/test_MAE.py
@@ -59,7 +59,7 @@ def test():
     print(probs.shape)
     print(labels.shape)
     np.save('prob.npy', probs)
-    preds = probs > 0.5;
+    preds = probs > 0.38;
     print(preds)
     print(accuracy_score(labels, preds), precision_score(labels, preds), recall_score(labels, preds), f1_score(labels, preds))
 
